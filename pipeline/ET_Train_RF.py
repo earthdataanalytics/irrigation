bf0fc13dcb90476e828c4840985641db2a27392f
Blair Jones <bjonesneu@gmail.com>
2022-03-20 12:57:40 -0500

    working version

--- pipeline/ET_Train_RF.py
+++ pipeline/ET_Train_RF.py
@@ -41,8 +41,10 @@ import numpy as np
 import pandas as pd
 import json
 
+from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score
 
 import os
 import joblib
@@ -66,10 +68,11 @@ def main(datafile=None,
         save_model=True):
     data_filename = datafile
 
-    if not os.path.exists(inpath + data_filename):
+    infilename = data_filename.split('.')[0] + '/features.pkl'
+    if not os.path.exists(inpath + infilename):
         print('Missing input datafile')
 
-    df = pd.read_pickle(inpath + data_filename)
+    df = pd.read_pickle(inpath + infilename)
 
     out_foldername = data_filename.split('.')[0]
     path = outpath + out_foldername + '/'
@@ -96,13 +99,17 @@ def main(datafile=None,
     # ## Apply filters
 
     # ##### Remove low NDVI
+    out_stats['filter_ndvi'] = False
     if filter_ndvi:
+        out_stats['filter_ndvi'] = True
         out_stats['num_ndvi_filtered_out_rf'] = int((df.NDVI >= 0.2).sum())
         df = df[df.NDVI >= 0.2] # keep data points where NDVI > threshold
                                 # threshold discussed in team meetings on 22 & 25 Feb 2022
 
     # ##### Retain data when no precipitation in prior X days
+    out_stats['filter_rain'] = False
     if filter_rain:
+        out_stats['filter_rain'] = True
         out_stats['num_rain_filtered_out_rf'] = int((df.last_rain > 3).sum())
         df = df[df.last_rain > 3] # threshold defined in team meeting on 22 Feb 2022
 
@@ -110,14 +117,18 @@ def main(datafile=None,
     # ## Train model
 
     # ##### Setup columns to use
-    cols = ['ET_24h', 'NDVI', 'LandT_G', 'last_rain', 'sum_precip_priorX', 'mm', 'yyyy', 'loc_idx', 'date']
+    et_var = 'ET_24h'
     if calc_ET_region:
-        cols[0] = = 'ET_24h_R'
+        et_var = 'ET_24h_R'
+    out_stats['et_var'] = et_var
+    out_stats['temporality'] = 'all_months'
+
+    cols = [et_var, 'NDVI', 'LandT_G', 'last_rain', 'sum_precip_priorX', 'mm', 'yyyy', 'loc_idx', 'date']
     num_cols_rf = 6
 
     # ##### Setup training/validation dataset
     train_val_data = df.dropna(subset=cols)
-    out_stats['num_samples_train_total'] = int(len(train_val_data)))
+    out_stats['num_samples_train_total'] = int(len(train_val_data))
 
     X = train_val_data[cols]
     y = train_val_data['type']
@@ -125,22 +136,86 @@ def main(datafile=None,
     strat = train_val_data[['type']]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=strat)
 
-    out_stats['num_samples_train_irr'] = int(len(y_train[y_train == 'Irrigated'])))
-    out_stats['num_samples_train_rain'] = int(len(y_train[y_train=='Rainfed'])))
+    out_stats['num_samples_train_irr'] = int(len(y_train[y_train == 'Irrigated']))
+    out_stats['num_samples_train_rain'] = int(len(y_train[y_train=='Rainfed']))
 
     out_stats['num_samples_test_irr'] = int((y_test == 'Irrigated').sum())
     out_stats['num_samples_test_rain'] = int((y_test == 'Rainfed').sum())
 
+    # ##### Create and train model
     classifier = RandomForestClassifier()
     classifier.fit(X_train[cols[:num_cols_rf]], y_train)
 
+    # ##### Score model
     X_test_sub = X_test[cols[:num_cols_rf]]
-    out_stats['score_rf'] = float(classifier.score(X_test_sub, y_test))
+    out_stats['rf_accuracy'] = float(classifier.score(X_test_sub, y_test))
+
+    y_pred = classifier.predict(X_test_sub)
+    f1_rf = f1_score(y_test, y_pred, pos_label='Irrigated')
+    out_stats['rf_f1'] = float(f1_rf)
+
+
+    # ## Create plots
+
+    # ##### Confusion Matrix
+    cm_plot = ConfusionMatrixDisplay.from_estimator(classifier, X_test_sub, y_test, normalize='true', values_format='.2f')
+    cm_plot.plot()
+    plt.savefig(path + 'rf_cm.png')
+
+    # ##### Decision Tree
+    fig = plt.figure(figsize=(25,20))
+    dt_plot = tree.plot_tree(classifier.estimators_[0], feature_names=cols, class_names=y_test.unique(),
+                   max_depth=3, proportion=False, rounded=True, filled = True)
+    plt.savefig(path + 'rf_tree.png')
+    plt.clf()
+
+    # ##### Histogram of False Negatives
+    fig = plt.figure(figsize=(10,5))
+    mask1 = y_test != y_pred
+    mask2 = y_test == 'Irrigated'
+    false_neg_idx = X_test[mask1 & mask2].index
+    fn_plot = train_val_data.loc[false_neg_idx].sort_values('loc_idx').groupby('loc_idx').count()[et_var].plot.bar()
+    plt.title('Histogram of False Negatives')
+    plt.savefig(path + 'rf_hist_fn_by_loc.png')
+
+        # by month
+    fn_plot = train_val_data.loc[false_neg_idx].sort_values('loc_idx').groupby('mm').count()[et_var].plot.bar()
+    plt.title('Histogram of False Negatives')
+    plt.savefig(path + 'rf_hist_fn_by_month.png')
+
+    # ##### Histogram of False Positives
+    mask2 = y_test != 'Irrigated'
+    false_pos_idx = X_test[mask1 & mask2].index
+    fp_plot = train_val_data.loc[false_pos_idx].sort_values('loc_idx').groupby('loc_idx').count()[et_var].plot.bar()
+    plt.title('Histogram of False Positives')
+    plt.savefig(path + 'rf_hist_fp_by_loc.png')
+
+        # by month
+    fp_plot = train_val_data.loc[false_pos_idx].sort_values('loc_idx').groupby('mm').count()[et_var].plot.bar()
+    plt.title('Histogram of False Negatives')
+    plt.savefig(path + 'rf_hist_fp_by_month.png')
+
+
+    # ## Save training and test data
+
+    # test data
+    if save_model:
+        out = X_test
+        out['true_label'] = y_test
+        out['pred_label'] = y_pred
+        out.to_pickle(path + 'testdata.pkl')
+
+    # training data
+    if save_model:
+        out = X_train
+        out['true_label'] = y_train
+        out['pred_label'] = classifier.predict(X_train[cols[:num_cols_rf]])
+        out.to_pickle(path + 'traindata.pkl')
 
 
     # ## Save trained model
     if save_model:
-        joblib.dump(classifier, outpath + data_filename + "_rf_model.pkl", compress=3)
+        joblib.dump(classifier, path + "rf_model.pkl", compress=3)
 
 
     # ## Save summary statistics
